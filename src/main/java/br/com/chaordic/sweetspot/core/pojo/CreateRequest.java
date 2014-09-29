9a85c27a372011d210b44596e8d1fadaa3614f40
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2014-09-28 21:44:27 -0300

    added ami, group, zone, blockDeviceMappings attributes to POST request

--- src/main/java/br/com/chaordic/sweetspot/core/pojo/CreateRequest.java
+++ src/main/java/br/com/chaordic/sweetspot/core/pojo/CreateRequest.java
@@ -8,7 +8,11 @@ public class CreateRequest {
     public int numInstances;
     public String instanceType;
     public String instanceNamePrefix;
+    public String ami;
+    public String group;
+    public String zone;
     public Map<String, String> instanceTags;
+    public Map<String, String> blockDeviceMappings;
 
     public String getId() {
         return id;
@@ -43,5 +47,29 @@ public class CreateRequest {
     }
     public void setInstanceTags(Map<String, String> instanceTags) {
         this.instanceTags = instanceTags;
+    }
+    public String getAmi() {
+        return ami;
+    }
+    public void setAmi(String ami) {
+        this.ami = ami;
+    }
+    public String getGroup() {
+        return group;
+    }
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    public String getZone() {
+        return zone;
+    }
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+    public Map<String, String> getBlockDeviceMappings() {
+        return blockDeviceMappings;
+    }
+    public void setBlockDeviceMappings(Map<String, String> blockDeviceMappings) {
+        this.blockDeviceMappings = blockDeviceMappings;
     }
 }
